1a03d2d26e6ec6b3ad6674e3c1dcef8110d961b4
dsrojass1 <ds.rojass1@uniandes.edu.co>
2024-03-14 16:42:18 -0500

    manejador app creado y configurado

---
+++ manejador_presentacion_aceptacion/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ManejadorPresentacionAceptacionConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'manejador_presentacion_aceptacion'
