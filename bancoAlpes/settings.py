0665ae9c9ee49769cbcacd8855bb826d2fdbc68d
o.urregov <o.urregov@uniandes.edu.co>
2024-04-13 09:42:30 -0500

    cambios

--- bancoAlpes/settings.py
+++ bancoAlpes/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'manejador_presentacion_aceptacion',
+    'manejador_validacion',
     'rest_framework'
 ]
 
