4f5952b163f8dcfad6b4f0bd0072486c64f0b618
o.urregov <o.urregov@uniandes.edu.co>
2024-04-12 19:15:03 -0500

    transaccion model

--- manejador_validacion/models.py
+++ manejador_validacion/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from manejador_presentacion_aceptacion.models import Cliente
+
+class Transaccion(models.Model):
+    cuenta_origen = models.CharField(max_length=16)
+    titular_origen = models.ForeignKey(Cliente, on_delete=models.CASCADE, default=None)
+    cuenta_destino = models.CharField(max_length=16)
+    titular_destino = models.ForeignKey(Cliente, on_delete=models.CASCADE, default=None)
+    monto = models.DecimalField(max_digits=10, decimal_places=2)
+    dateTime = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return 'titular origen = %s; titular destino = %s' % (self.titular_origen, self.titular_destino) 
